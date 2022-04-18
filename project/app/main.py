338d182fb0fea71536fa339294276321bd8d9439
Scott Ellis <ellis.lowell.scott@gmail.com>
2022-04-18 11:31:01 -0700

    up to postgres setup, but not table created

--- project/app/main.py
+++ project/app/main.py
@@ -1,4 +1,7 @@
+import os
+
 from fastapi import FastAPI, Depends
+from tortoise.contrib.fastapi import register_tortoise
 
 from app.config import get_settings, Settings
 
@@ -6,8 +9,17 @@ from app.config import get_settings, Settings
 app = FastAPI()
 
 
+register_tortoise(
+    app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
+
+
 @app.get("/ping")
-def pong(settings: Settings = Depends(get_settings)):
+async def pong(settings: Settings = Depends(get_settings)):
     return {
         "ping": "pong!",
         "environment": settings.environment,
